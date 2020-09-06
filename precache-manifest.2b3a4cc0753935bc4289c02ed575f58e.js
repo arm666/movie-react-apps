06f75a6faabd2f0ee5567cc18373701ccc970dea
gorkhali <gorkhali9999@gmail.com>
2020-09-06 15:55:15 +0545

    Updates

---
+++ precache-manifest.2b3a4cc0753935bc4289c02ed575f58e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfff0559e9090525afcef5f0970cb75b",
+    "url": "/movie-react-apps/index.html"
+  },
+  {
+    "revision": "47421570276d0ff04fde",
+    "url": "/movie-react-apps/static/css/main.a9355773.chunk.css"
+  },
+  {
+    "revision": "d0283b9a41a7a482418b",
+    "url": "/movie-react-apps/static/js/2.6f4837d5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie-react-apps/static/js/2.6f4837d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47421570276d0ff04fde",
+    "url": "/movie-react-apps/static/js/main.48a1ea97.chunk.js"
+  },
+  {
+    "revision": "6137fc12011d183d4d74",
+    "url": "/movie-react-apps/static/js/runtime-main.11870403.js"
+  },
+  {
+    "revision": "47675147457cbc7e081e0fd3278318a0",
+    "url": "/movie-react-apps/static/media/arrow-right.47675147.svg"
+  }
+]);
